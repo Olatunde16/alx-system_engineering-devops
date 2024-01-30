c3ed47b63f49d8e6f171edf7a9495a7fac72dca2
Olutunmibi Iseoluwa Olatunde <125552420+Olatunde16@users.noreply.github.com>
2024-01-30 09:43:11 +0100

    Update 6-phone_number.rb

--- 0x06-regular_expressions/6-phone_number.rb
+++ 0x06-regular_expressions/6-phone_number.rb
@@ -1,13 +1,2 @@
 #!/usr/bin/env ruby
-
-# Check if an argument is provided
-if ARGV.empty?
-  puts "Usage: #{$PROGRAM_NAME} <string>"
-  exit 1
-end
-
-# Extract the argument
-input_string = ARGV[0]
-
-# Use the regular expression to find and output the matched phone number or an empty string if no match
-puts input_string.scan(/^\d{10}$/).join
+puts ARGV[0].scan(/^\d{10,10}$/).join
