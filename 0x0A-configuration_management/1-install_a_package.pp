e8dc657a3160ab2351c0b17f49504af63b13880b
Olutunmibi Iseoluwa Olatunde <125552420+Olatunde16@users.noreply.github.com>
2024-03-22 14:22:14 +0100

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,6 @@
+#!/usr/bin/pup
 # Install an especific version of flask (2.1.0)
-exec { 'install_flask':
-  command     => '/usr/bin/pip3 install Flask==2.1.0',
-  path        => ['/usr/bin'],
-  environment => ['PATH=/usr/bin'],
-  unless      => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
+package {'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
